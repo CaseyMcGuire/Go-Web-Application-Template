8017670df854b7f6ef5e54166d22eb57f375298d
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-05-18 10:55:03 -0700

    Modify authentication middleware.

--- src/server/middleware/auth_middleware.go
+++ src/server/middleware/auth_middleware.go
@@ -6,21 +6,31 @@ import (
 	"net/http"
 )
 
+var (
+	UserId = "UserId"
+)
+
 func AuthMiddleware(next http.Handler, u *util.UserSessionManager) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// Check if the user is authenticated
 		isLoggedIn, loggedInErr := u.IsUserLoggedIn(r)
-		userId, userIdErr := u.GetUserId(r)
-		if loggedInErr != nil || userIdErr != nil {
+		if loggedInErr != nil {
 			http.Redirect(w, r, "/500", http.StatusInternalServerError)
 			return
-		} else if !isLoggedIn || userId == -1 {
+		}
+
+		userId, userIdErr := u.GetUserId(r)
+		if userIdErr != nil {
+			http.Redirect(w, r, "/500", http.StatusInternalServerError)
+			return
+		}
+
+		if !isLoggedIn || userId == -1 {
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), "userID", userId)
+		ctx := context.WithValue(r.Context(), UserId, userId)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
